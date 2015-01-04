fa77e15ee732c8149dea51103bcdcc0074f146b9
HItesh Aneja <aneja_hitesh@yahoo.com>
2015-01-04 02:07:30 -0500

    Basic infra setup WIP.

---
+++ app.js
@@ -0,0 +1,89 @@
+'use strict';
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Require all relevant modules.
+var express = require('express'),
+    config = require('./server/config/environment'),
+    favicon = require('serve-favicon'),
+    morgan = require('morgan'),
+    compression = require('compression'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    cookieParser = require('cookie-parser'),
+    errorHandler = require('errorhandler'),
+    path = require('path'),
+    passport = require('passport'),
+    session = require('express-session'),
+    mongoStore = require('connect-mongo')(session),
+    mongoose = require('mongoose');
+
+
+// Connect to database
+mongoose.connect(config.mongo.uri, config.mongo.options);
+
+// Populate DB with sample data
+if(config.seedDB) { require('./server/config/seed'); }
+
+// Setup server
+var app = express();
+
+// Set View Engine.
+app.set('views', config.root + '/server/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// Register all the middleware here.
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(cookieParser());
+app.use(passport.initialize());
+// Persist sessions with mongoStore
+// We need to enable sessions for passport twitter because its an oauth 1.0 strategy
+app.use(session({
+    secret: config.secrets.session,
+    resave: true,
+    saveUninitialized: true,
+    store: new mongoStore({ mongoose_connection: mongoose.connection })
+}));
+var env = app.get('env');
+if ('production' === env) {
+    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    app.use(express.static(path.join(config.root, 'public')));
+    app.set('appPath', config.root + '/public');
+    app.use(morgan('dev'));
+}
+
+if ('development' === env || 'test' === env) {
+    app.use(require('connect-livereload')());
+    app.use(express.static(path.join(config.root, '.tmp')));
+    app.use(express.static(path.join(config.root, 'client')));
+    app.set('appPath', 'client');
+    app.use(morgan('dev'));
+    app.use(errorHandler()); // Error handler - has to be last
+}
+
+
+// Start the server and start listeneing on assinged port.
+var server = require('http').createServer(app);
+var socketio = require('socket.io')(server, {
+  serveClient: (config.env === 'production') ? false : true,
+  path: '/socket.io-client'
+});
+require('./server/config/socketio')(socketio);
+
+
+// Register all the application routes.
+require('./server/routes')(app);
+
+
+// Start server
+server.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;
